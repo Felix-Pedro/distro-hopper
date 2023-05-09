02fd138e3a388336e96aa2d50959076dbea7180b
Pedro Felix <pedrogomes.felix@usp.br>
2023-05-08 22:27:53 -0300

    adding error handling and logging capabilities

--- distro_hopper.jl
+++ distro_hopper.jl
@@ -1,1 +1,2 @@
 using Gtk
+using Logging
--- origin_scanner.jl
+++ origin_scanner.jl
@@ -1,45 +1,59 @@
+using Logging, LoggingExtras
+logger = SimpleLogger(stdout, Logging.Trace)
+addLogger(logger)
+push!(Loggers.handlers(logger), FileHandler("log.txt", force=true))
 
-function scan()
+function scan(pacman=true, flatpak=true, snap=true, appimage=true)
 
     apps = Dict()
 
     ######## Pacman ########
-
     pacman_packages = Vector{String}()
+    
+    try
+        cmd_output = read(`pacman -Q`, String)
 
-    cmd_output = read(`pacman -Q`, String)
-
-    # split the output by newline and store each line as an element in the vector
-    for package in split(cmd_output, '\n')
-        push!(pacman_packages, package)
+        # split the output by newline and store each line as an element in the vector
+        for package in split(cmd_output, '\n')
+            push!(pacman_packages, package)
+        end
+        apps["pacman"] = pacman_packages
+    catch e
+        @warn "trying to use pacman resulted in $e, prehaps pacman isn't available in your system"
     end
-    apps["pacman"] = pacman_packages
-
 
     ######## Flatpak ########
-
+    
     flatpak_packages = Vector{String}()
 
-    cmd_output = read(`flatpak list`, String)
+    try
+        cmd_output = read(`flatpak list`, String)
 
-    # split the output by newline and store each line as an element in the vector
-    for package in split(cmd_output, '\n')
-        push!(flatpak_packages, package)
+        # split the output by newline and store each line as an element in the vector
+        for package in split(cmd_output, '\n')
+            push!(flatpak_packages, package)
+        end
+        apps["flatpak"] = flatpak_packages
+    catch e  
+        @warn "trying to use flatpak resulted in $e, prehaps pacman isn't available in your system"
     end
-    apps["flatpak"] = flatpak_packages
 
     ######## Snap ########
 
     snap_packages = Vector{String}()
 
-    cmd_output = read(`snap list`, String)
+    try
+        cmd_output = read(`snap list`, String)
 
-    # split the output by newline and store each line as an element in the vector
-    for package in split(cmd_output, '\n')
-        push!(snap_packages, package)
+        # split the output by newline and store each line as an element in the vector
+        for package in split(cmd_output, '\n')
+            push!(snap_packages, package)
+        end
+
+        apps["snap"] = snap_packages
+    catch e
+        @warn "trying to use snap resulted in $e, prehaps pacman isn't available in your system"
     end
-
-    apps["snap"] = snap_packages
 
     ######## Appimage ########
 
